9e4895d28885afa3239568543b5e5d472f222821
Jacques Smuts <git@jacquessmuts.com>
2017-09-24 21:06:25 +0200

    Added proper fullscreen support to viewing videos

--- app/src/main/java/com/jacquessmuts/bakingforudacity/Activities/StepDetailActivity.java
+++ app/src/main/java/com/jacquessmuts/bakingforudacity/Activities/StepDetailActivity.java
@@ -2,14 +2,19 @@ package com.jacquessmuts.bakingforudacity.Activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.jacquessmuts.bakingforudacity.Fragments.StepDetailFragment;
 import com.jacquessmuts.bakingforudacity.Models.Recipe;
 import com.jacquessmuts.bakingforudacity.Models.Step;
 import com.jacquessmuts.bakingforudacity.R;
+import com.jacquessmuts.bakingforudacity.Utils.Util;
 
 import butterknife.ButterKnife;
 import icepick.Icepick;
@@ -35,6 +40,18 @@ public class StepDetailActivity extends AppCompatActivity implements StepDetailF
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (!Util.isTablet(this) && getResources().getConfiguration().orientation ==
+                Configuration.ORIENTATION_LANDSCAPE){
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+
         setContentView(R.layout.activity_step_detail);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ButterKnife.bind(this);
